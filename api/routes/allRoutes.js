d11dc685b2c6a17e3a7dc7a23f2c377a4bf662fa
maddiearogers <maddiearogers@gmail.com>
2019-10-22 20:00:55 -0700

    Added increment and decrement endpoints
    Added increment and decrement endpoints to update the number of cars in a lot by lotID

--- api/routes/allRoutes.js
+++ api/routes/allRoutes.js
@@ -1,37 +1,46 @@
 /*
-*   allRoutes.js
-*   This file sets up the routes for the LiveLotAppAPI
-*   Initially this file just has routes for the 'user' object, 'lot' object, and 'feedback' object, but all the routes for the application
-*   will eventually be written in here once the schemas and controllers are created.
-*/
-'use strict';
-module.exports = function (app) {
-    var user = require('../controllers/userCtrl');
-    var lot = require('../controllers/lotCtrl');
-    var feedback = require('../controllers/feedbackCtrl');
+ *   allRoutes.js
+ *   This file sets up the routes for the LiveLotAppAPI
+ *   Initially this file just has routes for the 'user' object, 'lot' object, and 'feedback' object, but all the routes for the application
+ *   will eventually be written in here once the schemas and controllers are created.
+ */
+"use strict";
+module.exports = function(app) {
+  var user = require("../controllers/userCtrl");
+  var lot = require("../controllers/lotCtrl");
+  var feedback = require("../controllers/feedbackCtrl");
 
-    // user Routes
-    app.route('/user')
-        .get(user.getAllUsers)
-        .post(user.createUser);
+  // user Routes
+  app
+    .route("/user")
+    .get(user.getAllUsers)
+    .post(user.createUser);
 
-    app.route('/user/:userId')
-        .get(user.getUser)
-        .put(user.updateUser)
-        .delete(user.deleteUser);
+  app
+    .route("/user/:userId")
+    .get(user.getUser)
+    .put(user.updateUser)
+    .delete(user.deleteUser);
 
-    // lot Routes
-    app.route('/lot')
-        .get(lot.getAllLots)
-        .post(lot.createLot);
+  // lot Routes
+  app
+    .route("/lot")
+    .get(lot.getAllLots)
+    .post(lot.createLot);
 
-    app.route('/lot/:lotId')
-        .get(lot.getLot)
-        .put(lot.updateLot)
-        .delete(lot.deleteLot);
+  app
+    .route("/lot/:lotId")
+    .get(lot.getLot)
+    .put(lot.updateLot)
+    .delete(lot.deleteLot);
 
-    // feedback Routes
-    app.route('/feedback')
-        .get(feedback.getAllFeedback)
-        .post(feedback.createFeedback);
+  // increment and decrement lots by ID
+  app.route("/increment/:lotId").put(lot.increment);
+  app.route("/decrement/:lotId").put(lot.decrement);
+
+  // feedback Routes
+  app
+    .route("/feedback")
+    .get(feedback.getAllFeedback)
+    .post(feedback.createFeedback);
 };
