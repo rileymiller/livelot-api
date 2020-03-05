5ba8f2abd5fcb03b6be2aa732cffb72b611a627a
Riley Miller <rmllr000@gmail.com>
2020-03-04 18:35:00 -0700

    no longer logging socket

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(
   (err, Database) => {
     io.on("connection", (socket) => {
 
-      console.log('client connected', socket)
+      console.log('client connected')
       socket.on('disconnect', (reason) => {
         // ...
         console.log('client diconnected, reason: ', reason)
